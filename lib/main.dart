f5b4886ce8e1bcbcd7fd6cb0fb24ddc0ff6db358
amandapalomo <amandapalomoolivares@gmail.com>
2021-11-26 16:02:47 +0100

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,5 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Uno',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Uno"),
-        ),
-      ),
-    );
-  }
 }
