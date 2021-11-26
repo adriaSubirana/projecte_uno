b7c494aa8e87a5bbbe937cfd6a8fd95be1107693
adriaSubirana <adria.subirana@estudiantat.upc.edu>
2021-11-26 17:19:11 +0100

    Mostrem un document a la pàgina inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,10 +19,26 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Uno"),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text("Uno"),
+          ),
+          body: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .doc("/Partidas/5k9aj8mcVC6X5FOldq8o")
+                .snapshots(),
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
+            ) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final doc = snapshot.data!;
+              final data = doc.data();
+
+              return Center(child: Text("${data?['turno']}"));
+            },
+          )),
     );
   }
 }
