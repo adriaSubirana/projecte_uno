1a4ad916671c94dc22cc37dcc21854e85473a710
Celeste Ortiz <maria.celeste.ortiz@estudiantat.upc.edu>
2021-11-29 16:58:52 +0100

    cambios en jugador

---
+++ lib/clases/Jugador.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+
+class Jugador {
+  late String nombre;
+  bool uno = false;
+  late bool host;
+  List<String> cartas = [];
+  Jugador(this.nombre, this.uno, this.host);
+}
+
+/*
+  //repartir
+  void repartir(List<String> baraja, List<Jugador> jugadores){
+    for(int i = 0, ){
+    if(jugadores[i].host == true){
+      
+    }} 
+  }
+
+
+//hacer el getters
+//hacer los setters (cambiar el valor)
+//función añadir carta a la lista de cartas del jugador
+cartas.add //función:añadir la carta robada a la lista de jugador
+*/
