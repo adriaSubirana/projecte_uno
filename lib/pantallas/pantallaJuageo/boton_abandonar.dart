992dcfad00f718a73db746dcb4199c551a8fd6b4
adriaSubirana <adria.subirana@estudiantat.upc.edu>
2021-12-03 17:06:22 +0100

    Boton abandonar sin accion

---
+++ lib/pantallas/pantallaJuageo/boton_abandonar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Abandonar extends StatefulWidget {
+  const Abandonar({Key? key}) : super(key: key);
+
+  @override
+  _AbandonarState createState() => _AbandonarState();
+}
+
+class _AbandonarState extends State<Abandonar> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("Salir"),
+              content: const Text("Seguro que quieres sali de la partida?"),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context, "Cancel"),
+                  child: const Text("Cancelar"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, "Salir");
+                  },
+                  child: const Text("Salir"),
+                )
+              ],
+            );
+          },
+        );
+      },
+      icon: const Icon(
+        Icons.close,
+        color: Colors.red,
+      ),
+    );
+  }
+}
