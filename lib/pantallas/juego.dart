04ee1eee65757c941c775316d9d4fd32ab26db3b
adriaSubirana <adria.subirana@estudiantat.upc.edu>
2021-12-20 17:09:44 +0100

    Cartas preciosas y conexión firestore a medias

--- lib/pantallas/juego.dart
+++ lib/pantallas/juego.dart
@@ -12,7 +12,7 @@ import 'package:projecte_uno/pantallas/pantallaJuego/boton_abandonar.dart';
 
 class PantallaJuego extends StatelessWidget {
   final mano = ["b0", "y4", "r8", "g7", "g9", "r3", "b2", "k%", "y5", "y4"];
-  final mesa = ["b1", "y5", "r9", "g8", "g0", "r4", "k3"];
+  final mesa = ["b1", "y5", "r9", "g8", "g0", "r4", "r3"];
 
   PantallaJuego({Key? key}) : super(key: key);
 
@@ -32,8 +32,6 @@ class PantallaJuego extends StatelessWidget {
             Jugador("lisa", false),
             Jugador("jamito", false),
             Jugador("bea", false),
-            Jugador("teo", false),
-            Jugador("andres", false)
           ]),
           Expanded(
             flex: 8,
@@ -49,9 +47,7 @@ class PantallaJuego extends StatelessWidget {
                       Spacer(),
                       UNO(j1: Jugador("pepe", true)),
                       Spacer(),
-                      Robar(
-                          j1: Jugador("pepe", true),
-                          p1: Partida(Jugador("elHost", true))),
+                      Robar(j1: Jugador("pepe", true), p1: Partida()),
                       Spacer(),
                     ],
                   ),
--- lib/pantallas/pantallaJuego/build_carta.dart
+++ lib/pantallas/pantallaJuego/build_carta.dart
@@ -24,67 +24,88 @@ class Carta extends StatelessWidget {
         child: FittedBox(
           child: Card(
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
+              borderRadius: BorderRadius.circular(30),
             ),
             elevation: 20.0,
-            color: cardColor[codigo[0]],
-            child: SizedBox(
-              height: 350,
-              width: 250,
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 24),
-                      child: Text(
-                        codigo[1],
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 48,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                        child: Center(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                elevation: 0,
+                color: cardColor[codigo[0]],
+                child: SizedBox(
+                  height: 350,
+                  width: 250,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 24),
                           child: Text(
                             codigo[1],
-                            style: TextStyle(
-                              color: cardColor[codigo[0]],
+                            style: const TextStyle(
+                              color: Colors.white,
                               fontWeight: FontWeight.bold,
-                              fontSize: 96,
+                              fontSize: 48,
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 24),
-                      child: Text(
-                        codigo[1],
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 48,
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Center(
+                          child: RotationTransition(
+                            turns: const AlwaysStoppedAnimation(45 / 360),
+                            child: ClipOval(
+                              child: Container(
+                                height: 320,
+                                width: 180,
+                                decoration: const BoxDecoration(
+                                  //shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                                child: Center(
+                                  child: RotationTransition(
+                                    turns:
+                                        const AlwaysStoppedAnimation(-45 / 360),
+                                    child: Text(
+                                      codigo[1],
+                                      style: TextStyle(
+                                        color: cardColor[codigo[0]],
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 96,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 24),
+                          child: Text(
+                            codigo[1],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 48,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
