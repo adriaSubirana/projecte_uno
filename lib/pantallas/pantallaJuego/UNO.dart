d52024550390c351750caac8f1d48451886b6aa2
Celeste Ortiz <maria.celeste.ortiz@estudiantat.upc.edu>
2021-12-20 15:47:58 +0100

    Actualización botón UNO

--- lib/pantallas/pantallaJuego/UNO.dart
+++ lib/pantallas/pantallaJuego/UNO.dart
@@ -15,20 +15,23 @@ class UNO extends StatefulWidget {
 
 class _UNOState extends State<UNO> {
   late bool _pulsado;
+  late bool _estado;
 
   @override
   void initState() {
     super.initState();
     _pulsado = false;
+    _estado = widget.j1.uno;
   }
 
   void _UNOPulsado() {
     setState(() {
-      _pulsado = !_pulsado;
       if (widget.j1.cartas.length == 1) {
         widget.j1.setUno(true);
+        _estado = widget.j1.getUno();
       } else {
         widget.j1.setUno(false);
+        _estado = widget.j1.getUno();
       }
     });
   }
@@ -36,32 +39,48 @@ class _UNOState extends State<UNO> {
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.all(12),
-        child: FloatingActionButton.extended(
-          splashColor: Colors.white,
-          onPressed: () {
-            _UNOPulsado();
+        child: GestureDetector(
+          onLongPress: () {
+            setState(() {
+              if (_estado == true) {
+                _pulsado = true;
+              } else {
+                _pulsado = false;
+              }
+            });
           },
-          // icon: Icon(Icons.),
-          label: Text(
-            "UNO",
-            style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 20,
-                shadows: [
-                  Shadow(
-                    color: Color.fromRGBO(255, 255, 0, 10),
-                    offset: Offset(0, 3),
-                    blurRadius: 3,
-                  )
-                ],
-                fontWeight: FontWeight.bold),
+          onLongPressUp: () {
+            setState(() {
+              _pulsado = false;
+            });
+          },
+          child: FloatingActionButton.extended(
+            splashColor: _estado ? Colors.red : Colors.blue,
+            onPressed: () {
+              _UNOPulsado();
+            },
+            // icon: Icon(Icons.),
+            label: Text(
+              "UNO",
+              style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 20,
+                  shadows: [
+                    Shadow(
+                      color: Color.fromRGBO(255, 255, 0, 10),
+                      offset: Offset(0, 3),
+                      blurRadius: 3,
+                    )
+                  ],
+                  fontWeight: FontWeight.bold),
+            ),
+            backgroundColor: _pulsado ? Colors.red : Colors.blue,
+            focusElevation: 3,
+            extendedPadding: EdgeInsets.all(8),
+            foregroundColor: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           ),
-          backgroundColor: _pulsado ? Colors.blue : Colors.red,
-          focusElevation: 3,
-          extendedPadding: EdgeInsets.all(8),
-          foregroundColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         ));
   }
 }
