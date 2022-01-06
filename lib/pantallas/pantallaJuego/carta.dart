5fc50b8a52aae8da35bc569238aa120fb28fdf6d
adriaSubirana <adria.subirana@estudiantat.upc.edu>
2022-01-06 12:56:58 +0100

    Carta mejorada

--- lib/pantallas/pantallaJuego/carta.dart
+++ lib/pantallas/pantallaJuego/carta.dart
@@ -8,6 +8,96 @@ const Map<String, Color> cardColor = {
   'k': Colors.black,
 };
 
+class Carta extends StatelessWidget {
+  final String codigo;
+  final void Function(String)? onPressed; // una funció: void f()
+
+  const Carta({
+    Key? key,
+    required this.codigo,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onPressed != null) onPressed!(codigo);
+      },
+      child: AspectRatio(
+        aspectRatio: 2.5 / 3.5,
+        child: FittedBox(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            elevation: 20.0,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                elevation: 0,
+                color: cardColor[codigo[0]],
+                child: SizedBox(
+                  height: 350,
+                  width: 250,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 16),
+                            child: cardSymbol(codigo, Colors.white)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Center(
+                          child: RotationTransition(
+                            turns: const AlwaysStoppedAnimation(40 / 360),
+                            child: ClipOval(
+                              child: Container(
+                                height: 320,
+                                width: 195,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Center(
+                                  child: RotationTransition(
+                                    turns:
+                                        const AlwaysStoppedAnimation(-40 / 360),
+                                    child: cardSymbol(
+                                        codigo, cardColor[codigo[0]]!),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 16),
+                          child: cardSymbol(codigo, Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 Widget cardSymbol(String s, Color c) {
   final double size = c == Colors.white ? 56 : 112;
   if (s[1] == '#') {
@@ -154,7 +244,57 @@ Widget cardSymbol(String s, Color c) {
               ],
             );
     }
-    // TODO: Simbolo cambio de color
+  } else if (s[1] == '@') {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size == 56 ? 8 : 0),
+      child: RotationTransition(
+        turns: const AlwaysStoppedAnimation(40 / 360),
+        child: ClipOval(
+          child: SizedBox(
+            height: size == 56 ? 60 : 300,
+            width: size == 56 ? 35 : 175,
+            child: Center(
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      color: Colors.red,
+                      height: size == 56 ? 60 / 2 : 300 / 2,
+                      width: size == 56 ? 35 / 2 : 175 / 2,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      color: Colors.blue,
+                      height: size == 56 ? 60 / 2 : 300 / 2,
+                      width: size == 56 ? 35 / 2 : 175 / 2,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      color: Colors.amber,
+                      height: size == 56 ? 60 / 2 : 300 / 2,
+                      width: size == 56 ? 35 / 2 : 175 / 2,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      color: Colors.green,
+                      height: size == 56 ? 60 / 2 : 300 / 2,
+                      width: size == 56 ? 35 / 2 : 175 / 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   } else {
     return Text(
       " ${s[1]} ",
@@ -162,96 +302,6 @@ Widget cardSymbol(String s, Color c) {
         color: c,
         fontWeight: FontWeight.bold,
         fontSize: size,
-      ),
-    );
-  }
-}
-
-class Carta extends StatelessWidget {
-  final String codigo;
-  final void Function(String)? onPressed; // una funció: void f()
-
-  const Carta({
-    Key? key,
-    required this.codigo,
-    this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (onPressed != null) onPressed!(codigo);
-      },
-      child: AspectRatio(
-        aspectRatio: 2.5 / 3.5,
-        child: FittedBox(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            elevation: 20.0,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                elevation: 0,
-                color: cardColor[codigo[0]],
-                child: SizedBox(
-                  height: 350,
-                  width: 250,
-                  child: Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 16, horizontal: 16),
-                            child: cardSymbol(codigo, Colors.white)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Center(
-                          child: RotationTransition(
-                            turns: const AlwaysStoppedAnimation(40 / 360),
-                            child: ClipOval(
-                              child: Container(
-                                height: 320,
-                                width: 195,
-                                decoration: const BoxDecoration(
-                                  //shape: BoxShape.circle,
-                                  color: Colors.white,
-                                ),
-                                child: Center(
-                                  child: RotationTransition(
-                                    turns:
-                                        const AlwaysStoppedAnimation(-40 / 360),
-                                    child: cardSymbol(
-                                        codigo, cardColor[codigo[0]]!),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 16, horizontal: 16),
-                            child: cardSymbol(codigo, Colors.white)),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
