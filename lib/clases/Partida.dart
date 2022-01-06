0c19674251d16da3b8b679a92e2fc6dfb04ace08
adriaSubirana <adria.subirana@estudiantat.upc.edu>
2022-01-06 14:18:02 +0100

    - Renovar cartas - Cambiar Partida para saber si tienes que robar i implementacion cambio de color - Tomar cartas en juego - Cuando abandonas como no host tus cartas van a cartasRobar - Cuando hay cambio de color sabes que color tienes que tirar

--- lib/clases/Partida.dart
+++ lib/clases/Partida.dart
@@ -7,6 +7,8 @@ class Partida {
   late List<String> cartasMesa, cartasRobar;
   late bool enCurso;
   late int turno, sentido;
+  late String color;
+  late int tomar;
 
   Partida() {
     cartasMesa = [];
@@ -15,6 +17,8 @@ class Partida {
     enCurso = false;
     turno = 0;
     sentido = 1;
+    color = '';
+    tomar = 0;
   }
 
   Partida.fromFirestore(Map<String, dynamic> data)
@@ -22,18 +26,28 @@ class Partida {
         cartasRobar = (data['cartasRobar'] as List).cast<String>(),
         enCurso = data['enCurso'],
         turno = data['turno'],
-        sentido = data['sentido'];
+        sentido = data['sentido'],
+        color = data['color'],
+        tomar = data['tomar'];
 
   Map<String, dynamic> toFirestore() => {
         'cartasMesa': cartasMesa,
         'cartasRobar': cartasRobar,
         'enCurso': enCurso,
         'turno': turno,
-        'sentido': sentido
+        'sentido': sentido,
+        'color': color,
+        'tomar': tomar
       };
 
   void cambioSentido() {
     sentido = -sentido;
+  }
+
+  String robar() {
+    final c = cartasRobar[0];
+    cartasRobar.removeAt(0);
+    return c;
   }
 }
 
@@ -52,32 +66,6 @@ Stream<DocumentSnapshot<Partida>> partidaSnapshots(String id) {
   // void eliminarJugador(int i) => jugadores.removeAt(i);
 
   // void addCartaMesa(String c) => cartasMesa.add(c);
-
-//   String robar() {
-//     return cartasRobar[0];
-//     cartasRobar.removeAt(0);
-//   }
-
-//   void renovar() {
-//     for (int i = cartasMesa.length - 1; i > 0; i--) {
-//       cartasRobar.add(cartasMesa[i]);
-//       cartasMesa.removeAt(i);
-//     }
-//     cartasRobar.shuffle();
-//   }
-
-//   void siguienteTurno() {
-//     if (turno == jugadores.length - 1) {
-//       turno = 0;
-//     } else {
-//       turno++;
-//     }
-//   }
-
-//   void shuffleTurno() {
-//     var rand = Random();
-//     turno = rand.nextInt(jugadores.length);
-//   }
 
 //   void repartir() {
 //     for (int i = 0; i < jugadores.length; i++) {
